3147856e2f7afb5632457291d149a54db103c298
PoornimaPalliyaGuruge <86935934+PoornimaPalliyaGuruge@users.noreply.github.com>
2024-03-19 23:05:40 +0530

    course API added

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const bp = require("body-parser");
 const passport = require("passport");
 const { connect } = require("mongoose");
 const { success, error } = require("consola");
+const courseRoute = require("./routes/courses.route.js")
 
 // Bring in the app constants
 const { DB, PORT } = require("./config");
+const Course = require("./models/Course.js");
 
 // Initialize the application
 const app = exp();
@@ -20,6 +22,8 @@ require("./middlewares/passport");
 
 //use router middleware
 app.use("/api/users", require("./routes/users"));
+app.use("/api/courses", courseRoute);
+
 
 // Connection with DB
 const startApp = async () => {
@@ -27,21 +31,21 @@ const startApp = async () => {
     await connect(DB, {
       serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds of inactivity
       useNewUrlParser: true, // Optional. Necessary for version 4.0.0 and above
-      useUnifiedTopology: true // Optional. Necessary for version 4.0.0 and above
+      useUnifiedTopology: true, // Optional. Necessary for version 4.0.0 and above
     });
 
     success({
       message: `Successfully connected with the Database \n ${DB}`,
-      badge: true
+      badge: true,
     });
-//Start listen for the server on port
+    //Start listen for the server on port
     app.listen(PORT, () =>
       success({ message: `Server is running on port ${PORT}`, badge: true })
     );
   } catch (err) {
     error({
       message: `Unable to connect with DataBase \n${err}`,
-      badge: true
+      badge: true,
     });
     startApp();
   }
