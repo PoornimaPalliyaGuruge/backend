19488511861f7733156ef6676d55bf7b550ae1c5
PoornimaPalliyaGuruge <86935934+PoornimaPalliyaGuruge@users.noreply.github.com>
2024-03-24 19:05:56 +0530

    add course route

--- routes/courses.route.js
+++ routes/courses.route.js
@@ -1,16 +1,19 @@
-const exp = require("express");
-const router = require( "express" ).Router();
-const { getCourses, getCourse,createCourse, updateCourse, deleteCourse } =  require('../controllers/course.controller.js');
+    const exp = require("express");
+    const router = require( "express" ).Router();
+    const { getCourses, getCourse,createCourse, updateCourse, deleteCourse , assignFacultyToCourse} =  require('../controllers/course.controller.js');
+    const { userAuth, checkRole } = require('../controllers/Auth.js');
 
-router.get("/",getCourses);
+    router.get("/",getCourses);
 
-router.get("/:id",getCourse);
+    router.get("/:id",getCourse);
 
-router.post("/",createCourse);
+    router.post("/",createCourse);
 
-router.put("/:id",updateCourse);
+    router.put("/:id",updateCourse);
 
-router.delete("/:id", deleteCourse);
+    router.delete("/:id",deleteCourse);
 
-module.exports =  router;
-               
+    router.post('/:id/assign-faculty', userAuth, checkRole(['admin']), assignFacultyToCourse); 
+
+    module.exports =  router;
+                
