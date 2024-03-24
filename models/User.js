7602876178662f0e42ebe74800f4f8f671e1dcd8
PoornimaPalliyaGuruge <86935934+PoornimaPalliyaGuruge@users.noreply.github.com>
2024-03-24 19:03:50 +0530

    Add user REST API

--- models/User.js
+++ models/User.js
@@ -1,31 +1,31 @@
-const { Schema, model } = require('mongoose');
+    const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema(
-    {
-        name : {
-            type: String,
-            required : true
+    const UserSchema = new Schema(
+        {
+            name : {
+                type: String,
+                required : true
+            },
+            email :{
+                type: String,
+                required :true
+            },
+            role: {
+                type: String,
+                default : "student",
+                enum: ["student","faculty","admin"]
+            },
+            username:{
+                type: String,
+                required : true
+            },
+            password:{
+                type: String,
+                required : true
+            }
         },
-        email :{
-            type: String,
-            required :true
-        },
-        role: {
-            type: String,
-            default : "student",
-            enum: ["student","faculty","admin"]
-        },
-        username:{
-            type: String,
-            required : true
-        },
-        password:{
-            type: String,
-            required : true
-        }
-    },
-    { timestamps:true }
-    
-);
+        { timestamps:true }
+        
+    );
 
-module.exports =model("users",UserSchema) ; 
+    module.exports =model("users",UserSchema) ; 
--- routes/users.js
+++ routes/users.js
@@ -1,59 +1,59 @@
-const router = require( "express" ).Router();
-//Bring in the User Registration function
-const { userRegister, userLogin , userAuth, serialyzeUser,checkRole } = require( "../controllers/Auth" );
+    const router = require( "express" ).Router();
+    //Bring in the User Registration function
+    const { userRegister, userLogin , userAuth, serialyzeUser,checkRole } = require( "../controllers/Auth" );
 
 
-//Student regiatration route 
-router.post("/register-student", async( req,res ) => {
-    await userRegister(req.body, 'student',res);
-});
+    //Student regiatration route 
+    router.post("/register-student", async( req,res ) => {
+        await userRegister(req.body, 'student',res);
+    });
 
-//Admin registration route 
-router.post("/register-admin", async( req,res ) => {
-    await userRegister(req.body, 'admin',res);
-});
+    //Admin registration route 
+    router.post("/register-admin", async( req,res ) => {
+        await userRegister(req.body, 'admin',res);
+    });
 
-//Faculty registration route
-router.post("/register-faculty", async( req,res ) => {
-    await userRegister(req.body, 'faculty',res);
-});
-
-
-//Student Login route 
-router.post("/login-student", async( req,res ) => {
-    await userLogin(req.body,'student',res)
-});
-//Admin Login route 
-router.post("/login-admin", async( req,res ) => {
-    await userLogin(req.body,'admin',res)
-});
-//Faculty Login route
-router.post("/login-faculty", async( req,res ) => {
-    await userLogin(req.body,'faculty',res)
-});
+    //Faculty registration route
+    router.post("/register-faculty", async( req,res ) => {
+        await userRegister(req.body, 'faculty',res);
+    });
 
 
-//profile route
-router.get("/profile", userAuth, async( req, res )=>{
-    return res.json(serialyzeUser(res.user));
-});
+    //Student Login route 
+    router.post("/login-student", async( req,res ) => {
+        await userLogin(req.body,'student',res)
+    });
+    //Admin Login route 
+    router.post("/login-admin", async( req,res ) => {
+        await userLogin(req.body,'admin',res)
+    });
+    //Faculty Login route
+    router.post("/login-faculty", async( req,res ) => {
+        await userLogin(req.body,'faculty',res)
+    });
 
 
-//Student protected route 
-router.get("/student-protected",userAuth,checkRole(["student"]) , async( req,res ) => {
-   return res.json("Hello Student!!");
-});
-//Admin protected route 
-router.get("/admin-protected",userAuth, checkRole(["admin"]) , async( req,res ) => {
-    return res.json("Hello Admin!!");
-});
-//Faculty protected route
-router.get("/faculty-protected",userAuth, checkRole(["faculty"]) , async( req,res ) => {
-    return res.json("Hello Faculty!!");
-});
+    // profile route
+    router.get("/profile", userAuth, async (req, res) => {
+        return res.json(serialyzeUser(req.user));
+    });
 
-router.get("/admin-protected-and-faculty-protected",userAuth, checkRole(["admin","faculty"]) , async( req,res ) => {
-    return res.json("Hello Admin and faculty!!");
-});
 
-module.exports = router;
+    //Student protected route 
+    router.get("/student-protected",userAuth,checkRole(["student"]) , async( req,res ) => {
+    return res.json("Hello Student!!");
+    });
+    //Admin protected route 
+    router.get("/admin-protected",userAuth, checkRole(["admin"]) , async( req,res ) => {
+        return res.json("Hello Admin!!");
+    });
+    //Faculty protected route
+    router.get("/faculty-protected",userAuth, checkRole(["faculty"]) , async( req,res ) => {
+        return res.json("Hello Faculty!!");
+    });
+
+    router.get("/admin-protected-and-faculty-protected",userAuth, checkRole(["admin","faculty"]) , async( req,res ) => {
+        return res.json("Hello Admin and faculty!!");
+    });
+
+    module.exports = router;
